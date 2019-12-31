351832c1394e3d8abe97c1eb650c1d2788901d3e
kanakmishra <kanak.mishra@systematixindia.com>
2019-12-31 12:10:51 +0530

    takin latest pull

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -67,10 +67,10 @@
 
 <body>
 
-    <div class="header">
+    <div class="header fixed">
         <a href="#default" class="logo">Learning Application</a>
         <div class="header-right">
-            <a href="#">Home</a>
+            <a href="/">Home</a>
             <a href="/category">Add Category</a>
             <a href="/sub-category">Add Subcategory</a>
             <a href="/add-question">Add Question</a>
